d06da2f3c309e82129060b0b497e415db5152c7c
Arthur Kozhemyakin <trable228@icloud.com>
2025-02-13 00:53:56 +0300

    add btn for get and set last name

--- app.js
+++ app.js
@@ -8,21 +8,54 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Данные авторизации:", authData);
 
         // Обработчик кнопки "Получить задачи"
-        document.getElementById("actionButton").addEventListener("click", function () {
-            doAction();
+        document.getElementById("createTaskButton").addEventListener("click", function () {
+            makeTask();
+        });
+
+        // Обработчик кнопки "Получить фамилию"
+        document.getElementById("getLastNameButton").addEventListener("click", function () {
+            getLastName();
+        });
+
+        // Обработчик кнопки "Поменять фамилию"
+        document.getElementById("setLastNameButton").addEventListener("click", function () {
+            setLastName();
         });
     });
 });
 
 // Функция для получения списка задач
-function doAction() {
-    makeTask();
+function getLastName() {
+    BX24.callMethod("user.current", {}, function(result) {
+        if (result.error()) {
+            console.error(result.error());
+        } else {
+            console.log(result.data().LAST_NAME);
+        }
+    });
+}
+
+// Функция для изменения фамилии на "новая фамилия"
+function setLastName() {
+    BX24.callMethod("user.current", {}, function(result) {
+        if (result.error()) {
+            console.error(result.error());
+        } else {
+            console.log(result.data().LAST_NAME);
+            BX24.callMethod("user.update", {
+                ID: result.data().ID,
+                LAST_NAME: "новая фамилия"
+            }, function(result) {
+                console.log(result);
+            });
+        }
+    });
+
 }
 
 async function makeTask() {
     try {
         let id = await getUserId(); // Ждем получения ID
-        console.log(id); // Теперь id будет правильным
         BX24.callMethod("tasks.task.add", {
             fields: {
                 TITLE: "test",
@@ -46,7 +79,7 @@ async function getUserId() {
                     reject(result.error()); // Отклоняем промис в случае ошибки
                 } else {
                     const id = result.data().ID; // Получаем ID
-                    console.log(id);
+                    console.log("ID пользователя:", id);
                     resolve(id); // Разрешаем промис с ID
                 }
             }
